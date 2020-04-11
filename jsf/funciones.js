5fa813173f57a16f81dd1cdd66bf1e638db07b34
“nsandobal” <nicsandobal@gmail.com>
2020-04-11 04:50:11 -0300

    Filtro de fecha terminado

--- jsf/funciones.js
+++ jsf/funciones.js
@@ -2,6 +2,7 @@ function apretarBoton() {
 
 	var palabras_clave = String(document.getElementById('input-pc').value);
 	var autor = String(document.getElementById('input-autor').value);
+	var fecha = String(document.getElementById('input-date').value);
 	var divnoticia = document.getElementById('div-para-imprimir');
 	var hubo_resultados = false;
 
@@ -10,6 +11,15 @@ function apretarBoton() {
 		url += "q=" + palabras_clave + "&";
 	}
 	url += "country=ar";
+
+	var datenow = new Date();
+	var fecha_now = datenow.getFullYear() + "-" 
+
+	if (datenow.getMonth() < 10) { fecha_now += "0"; } 
+	fecha_now += ((datenow.getMonth()) + 1) + "-";
+	if (datenow.getDate() < 10) { fecha_now += "0"; } 
+	fecha_now += datenow.getDate();
+
 	url += '&apiKey=41145be57b964f11a90511f640fa2eec';
 	
 	var req = new Request(url);
@@ -21,51 +31,57 @@ function apretarBoton() {
 		var noticias = prueba.articles;
 		var lista_de_noticias = "";
 		if (noticias.length > 0) {
-			if (autor != "") {
+			if (autor != "" || fecha != "") {
 				// Creamos un vector de noticias filtradas por autor
 				var noticias_filtradas = [];
-				for (var n=0; n<noticias.length; n++){	
-					console.log(autor);
-					console.log(noticias[n].author);
-					if (noticias[n].author == autor) {
+				for (var n=0; n<noticias.length; n++){
+					if (autor != "" && noticias[n].author == autor && fecha == "") {
 						noticias_filtradas.push(noticias[n]);
+					}
+					else if (fecha != "" && (noticias[n].publishedAt).slice(0, 10) >= fecha && autor == "") {
+						noticias_filtradas.push(noticias[n]);
+					}
+					else {
+						if (noticias[n].author == autor && (noticias[n].publishedAt).slice(0, 10) >= fecha) {
+							noticias_filtradas.push(noticias[n]);
+						}
 					}
 				}
 				if (noticias_filtradas.length > 0) {
 					hubo_resultados = true;
-					console.log("hubo resultados jejejeje");
+					console.log("hubo resultados con alguno o ambos filtros jejejeje");
 					for ( var n=0; n<noticias_filtradas.length; n++){			
 						lista_de_noticias += '<div class="in-flex"><br>Autor: ' + noticias_filtradas[n].author;
 						lista_de_noticias += '<br>Titulo: ' + noticias_filtradas[n].title;
 						lista_de_noticias += '<br>Contenido ' + noticias_filtradas[n].content;
-						lista_de_noticias += '<br>Fecha: ' + noticias_filtradas[n].publishedAt;
+						lista_de_noticias += '<br>Fecha: ' + (noticias_filtradas[n].publishedAt).slice(0, 10);
 						lista_de_noticias += '<br>url: <a href=' + noticias_filtradas[n].url + ' target="_blank">Link a la noticia</a></div>';
 					}
 				}
 				else {
-					console.log("hubo noticias pero no de ese autor");
+					console.log("de tanto filtro dejamos cero");
 				}
-			}
+			}			
 			else {
 				hubo_resultados = true;
-				console.log("hubo resultados encima sin autor");
+				console.log("como no hubo filtros traemos todo");
 				for ( var n=0; n<noticias.length; n++){			
 					lista_de_noticias += '<div class="in-flex"><br>Autor: ' + noticias[n].author;
 					lista_de_noticias += '<br>Titulo: ' + noticias[n].title;
 					lista_de_noticias += '<br>Contenido ' + noticias[n].content;
-					lista_de_noticias += '<br>Fecha: ' + noticias[n].publishedAt;
+					lista_de_noticias += '<br>Fecha: ' + (noticias[n].publishedAt).slice(0, 10);
 					lista_de_noticias += '<br>url: <a href=' + noticias[n].url + ' target="_blank">Link a la noticia</a></div>';
 				}
 			}
 		}
 		else {
-			console.log("no hubo ni noticias genericas");
+			console.log("no hubo ni noticias");
 		}
 	if (hubo_resultados) {
 		divnoticia.innerHTML = lista_de_noticias;
 	}
 	else {
-		divnoticia.innerHTML = '<div class="in-flex">No hubo resultados</div>';
+		divnoticia.innerHTML = '<div class="in-flex">La búsqueda no arrojó resultados</div>';
 	}
 	
 	});
