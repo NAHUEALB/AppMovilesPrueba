ae7bc9cdc0feeb60c809d29010594b98b947c35d
unknown <nahuelalbertiutn@gmail.com>
2020-04-17 14:21:37 -0300

    Cierra sidebar automaticamente al tocar afuera del side pero dentro del center

---
+++ jsf/sidebar.js
@@ -0,0 +1,11 @@
+function ocultarSide(event){
+
+var check =  document.getElementById('check');
+//El checked se fija el valor del checkbox, si es true (osea que esta abierto) lo cierra
+
+    if(check.checked){
+        check.click();
+    }
+    
+
+}
